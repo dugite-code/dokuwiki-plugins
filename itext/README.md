ea30477326cb18f697914f5b917de828568f60d2
dugite-code <dugite-code@peekread.info>
2018-04-12 10:47:38 +0800

    Added itext plugin

---
+++ itext/README.md
@@ -0,0 +1,17 @@
+# Tab Plugin
+
+Modified from [Sample Plugin 2 - Color](https://www.dokuwiki.org/devel:syntax_plugins#sample_plugin_2_-_color) incorporating bits of [Tim Skoch's Tab Plugin](https://www.dokuwiki.org/plugin:tab)
+
+### Syntax:
+
+`<itext{% width}>`
+
+### Examples:
+
+`<itext> input text with no width </itext>`
+
+<input value='input text with no width'>
+
+`<itext> input text with 50% width </itext>`
+
+<input style='width 50%' value='input text with 25% width'>
---
+++ itext/syntax.php
@@ -0,0 +1,81 @@
+<?php
+/**
+* Plugin Color: Sets new colors for text and background.
+*
+* @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author     Christopher Smith <chris@jalakai.co.uk>
+*/
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+* All DokuWiki plugins to extend the parser/rendering mechanism
+* need to inherit from this class
+*/
+class syntax_plugin_itext extends DokuWiki_Syntax_Plugin {
+
+
+	/**
+	* return some info
+	*/
+	function getInfo(){
+		return array(
+			'author' => 'Dugite-Code',
+			'email'  => 'dugite-code@peekread.info',
+			'date'   => '2018-04-12',
+			'name'   => 'Itext Plugin',
+			'desc'   => 'Wraps text in an input box to make selecting/copying simpler',
+			'url'    => 'https://github.com/dugite-code/dokuwiki-plugins/tree/master/itext',
+		);
+	}
+
+
+	public function getType(){ return 'formatting'; }
+	public function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }
+	public function getSort(){ return 158; }
+	public function connectTo($mode) { $this->Lexer->addEntryPattern('<itext.*?>(?=.*?</itext>)',$mode,'plugin_itext'); }
+	public function postConnect() { $this->Lexer->addExitPattern('</itext>','plugin_itext'); }
+
+
+	/**
+	* Handle the match
+	*/
+	public function handle($match, $state, $pos, Doku_Handler $handler){
+		switch ($state) {
+			case DOKU_LEXER_ENTER :
+			$match = substr($match,6,-1);
+			if ((strlen($match)>0) && ($match >0)) {
+				$width = "style='width: ".$match."%'";
+			}
+			return array($state, $width);
+			case DOKU_LEXER_UNMATCHED :  return array($state, $match);
+			case DOKU_LEXER_EXIT :       return array($state, '');
+		}
+		return array();
+	}
+
+	/**
+	* Create output
+	*/
+	public function render($mode, Doku_Renderer $renderer, $data) {
+		// $data is what the function handle() return'ed.
+		if($mode == 'xhtml'){
+			/** @var Doku_Renderer_xhtml $renderer */
+			list($state,$match) = $data;
+			switch ($state) {
+				case DOKU_LEXER_ENTER :
+				$renderer->doc .= "<input $match value='";
+				break;
+				case DOKU_LEXER_UNMATCHED :
+				$renderer->doc .= $renderer->_xmlEntities($match);
+				break;
+				case DOKU_LEXER_EXIT :
+				$renderer->doc .= "'>";
+				break;
+			}
+			return true;
+		}
+		return false;
+	}
+}
